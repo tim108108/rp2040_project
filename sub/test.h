0d7c02511e2b93d0c2ca7b7aac031e1714196e0a
tim108108 <tim108108@gmail.com>
2023-04-05 16:21:02 +0800

    new

---
+++ sub/test.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#define ws2812_wrap_target 0
+#define ws2812_wrap 3
+#define ws2812_T1 2
+#define ws2812_T2 5
+#define ws2812_T3 3
+
+static const uint16_t ws2812_program_instructions[] = {
+            //     .wrap_target
+    0x6221, //  0: out    x, 1            side 0 [2] 
+    0x1123, //  1: jmp    !x, 3           side 1 [1] 
+    0x1400, //  2: jmp    0               side 1 [4] 
+    0xa442, //  3: nop                    side 0 [4] 
+            //     .wrap
+};
+
+static const struct pio_program ws2812_program = {
+    .instructions = ws2812_program_instructions,
+    .length = 4,
+    .origin = -1,
+};
+
+
+static inline pio_sm_config ws2812_program_get_default_config(uint);
+static inline void ws2812_program_init(PIO pio, uint sm, uint offset, uint pin, float freq, bool rgbw);
+void put_pixel(uint32_t pixel_grb);
+void put_rgb(uint8_t red, uint8_t green, uint8_t blue);
+void RP2040_zero_test_code();
